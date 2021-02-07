29035a938a220363611895284f08041d88bc9e38
GenElectrovise <sanchez.pamelos@outlook.com>
2021-02-07 11:00:40 +0000

    BizarreCmd run()

--- src/main/java/genelectrovise/hypixel/skyblock/bizarre/command/CmdBizarre.java
+++ src/main/java/genelectrovise/hypixel/skyblock/bizarre/command/CmdBizarre.java
@@ -11,7 +11,14 @@ public class CmdBizarre implements Runnable {
 	}
 
 	public void run() {
-		System.out.println("Use 'bizarre help' for help.");
+		System.out.println(" = \\ INFORMATION / = ");
+		System.out.println("Bizarre, by GenElectrovise");
+		System.out.println("Copyright (c) 2020 GenElectrovise");
+		System.out.println("An unofficial Bazaar analytics tool for Hypixel Skyblock.");
+		System.out.println("Open source on GitHub: https://github.com/GenElectrovise/Bizarre");
+		System.out.println("Licensed under GNU GPL-3.0. A copy of the license is packaged with this program, and can be read on the aforementioned GitHub repository, or on https://www.gnu.org/licenses/gpl-3.0.en.html");
+		System.out.println("Run this executable with the subcommand 'help' for help with commands.");
+		System.out.println(" = </\\> = ");
 	}
 
 }
