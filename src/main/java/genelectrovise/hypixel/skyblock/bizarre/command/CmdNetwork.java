2165bb2c95697b7b3d3ddbc8c1ee15eec54e0a84
GenElectrovise <sanchez.pamelos@outlook.com>
2021-02-09 15:54:45 +0000

    Added documentation for the repeating command task in CmdNetwork

--- src/main/java/genelectrovise/hypixel/skyblock/bizarre/command/CmdNetwork.java
+++ src/main/java/genelectrovise/hypixel/skyblock/bizarre/command/CmdNetwork.java
@@ -2,12 +2,34 @@ package genelectrovise.hypixel.skyblock.bizarre.command;
 
 import java.time.LocalDateTime;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
+/**
+ * Note:<br>
+ * The contained repeating task does:
+ * 
+ * <ol>
+ * <li>MAIN THREAD:
+ * <ol>
+ * <li>Fetch the latest data
+ * <li>Sort data
+ * <li>Create a new task (on a daemon thread) for each tracked item. PURPOSES MUST NEVER INTERSECT!!
+ * </ol>
+ * 
+ * <li>FOR EACH DAEMON THREAD:
+ * <ol>
+ * <li>Get the latest data for its item.
+ * <li>READ (not write) from the file system
+ * <li>Create a training set
+ * <li>Train the network
+ * </ol>
+ * </ol>
+ * 
+ */
 @Command(name = "network", description = "Interacts with Bizarre's neural networking capabilities.")
 public class CmdNetwork {
 
@@ -16,26 +38,10 @@ public class CmdNetwork {
 
 	@Command(name = "retrieve", description = "Starts the recurring task for retrieving Bazaar data. This task is only ended by closing the program.")
 	public void retrieve() {
-
 		Runnable command = new Runnable() {
 			@Override
 			public void run() {
 				System.out.println(LocalDateTime.now().toString() + " - Running task!!");
-
-				// OPEN THE DATABASE
-
-				// MAIN THREAD:
-				// - Fetch the latest data
-				// - Sort data
-				// - Create a new task for each tracked item. PURPOSES MUST NEVER INTERSECT!!
-
-				// FOR EACH DAEMON THREAD:
-				// - Get the latest data for its item.
-				// - READ (not write) from the file system
-				// - Create a training set
-				// - Train the network
-
-				// CLOSE THE DATABASE
 			}
 		};
 
@@ -43,10 +49,11 @@ public class CmdNetwork {
 		long period = 1;
 		TimeUnit unit = TimeUnit.MINUTES;
 
-		System.out.println("Created task for queuing to soon-to-create ScheduledThreadPoolExecutor (" + threads + " threads). With initialDelay=" + initialDelay + " period=" + period + " unit=" + unit.name());
+		System.out.println(
+				"Created task for queuing to soon-to-create ScheduledThreadPoolExecutor (" + threads + " threads). With initialDelay=" + initialDelay + " period=" + period + " unit=" + unit.name());
 
-		//Confimed cast
-		ScheduledThreadPoolExecutor service = (ScheduledThreadPoolExecutor) Executors.newScheduledThreadPool(threads);
+		// Confimed cast
+		ScheduledExecutorService service = (ScheduledExecutorService) Executors.newSingleThreadScheduledExecutor();
 		service.scheduleAtFixedRate(command, initialDelay, period, unit);
 		System.out.println("Started ScheduledThreadPoolExecutor @threads=" + threads + " @fixed-rate=" + period + unit.name().toLowerCase());
 	}
